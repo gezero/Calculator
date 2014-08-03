6dfb69c90528497de45943b2f56c77fab93d7221
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-08-03 18:00:35 +0200

    adding main

--- src/main/java/exam/Calculator.java
+++ src/main/java/exam/Calculator.java
@@ -1,5 +1,8 @@
 package exam;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -13,7 +16,7 @@ public class Calculator {
 
     public double calculate(List<Instruction> instructions) {
         validateScript(instructions);
-        double value = instructions.remove(instructions.size()-1).calculate(0d);
+        double value = instructions.remove(instructions.size() - 1).calculate(0d);
         for (Instruction instruction : instructions) {
             value = instruction.calculate(value);
         }
@@ -22,6 +25,39 @@ public class Calculator {
 
     private void validateScript(List<Instruction> instructions) {
         checkNotNull(instructions);
-        checkArgument(instructions.get(instructions.size()-1) instanceof Apply);
+        checkArgument(instructions.get(instructions.size() - 1) instanceof Apply);
+    }
+
+    public static void main(String[] args){
+        if (args.length != 1) {
+            help();
+            return;
+        }
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(args[0]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            help();
+            return;
+        }
+
+        Calculator calculator = new Calculator();
+
+        double result = 0;
+        try {
+            result = calculator.calculate(InstructionBuilder.readFromStream(inputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+            help();
+            return;
+        }
+        System.out.println(result);
+
+    }
+
+    private static void help() {
+        System.out.println("The program accepts only 1 parameter. It has to be a path to a file containing");
+        System.out.println("instructions that the calculator should calculate");
     }
 }
